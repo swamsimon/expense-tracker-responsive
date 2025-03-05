69f0554fb83157129c38e7d759ec4a4770d4ad9f
Swam Yee Zaw <swamsimon13@gmail.com>
2025-03-05 09:50:35 +0700

    Updated README file with new information

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';//sevices for setPreferredOrientations
+//sevices for setPreferredOrientations
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 96, 59, 181),
+  seedColor: const Color.fromARGB(255, 191, 4, 26),
 );
 
 var kDarkColorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
-  seedColor: const Color.fromARGB(255, 5, 99, 125),
+  seedColor: const Color.fromARGB(255, 194, 188, 2),
 );
 
 void main() {
